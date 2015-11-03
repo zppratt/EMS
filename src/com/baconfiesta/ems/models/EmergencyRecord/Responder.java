65b143e3eb4e35c98ae013227386e6695dfe2cd1
zppratt <zppratt@gmail.com>
2015-11-03 09:18:34 -0500

    Finished emergency record model Javadocs.

--- src/com/baconfiesta/ems/models/EmergencyRecord/Responder.java
+++ src/com/baconfiesta/ems/models/EmergencyRecord/Responder.java
@@ -1,12 +1,37 @@
 package com.baconfiesta.ems.models.EmergencyRecord;
 
+/**
+ * A responder to an emergency
+ */
 public class Responder {
 
+    /**
+     * The phone number of the responder
+     */
     private String phoneNumber;
+
+    /**
+     * The street address where the responder is located
+     */
     private String address;
+
+    /**
+     * The state where the responder is located
+     */
     private String state;
+
+    /**
+     * The zip code where the responder is located
+     */
     private int zip;
 
+    /**
+     * Default constructor for a responder object
+     * @param phoneNumber the phone number
+     * @param address the street address
+     * @param state the state
+     * @param zip the zip code
+     */
     public Responder(String phoneNumber, String address, String state, int zip) {
         this.phoneNumber = phoneNumber;
         this.address = address;
@@ -14,18 +39,34 @@ public class Responder {
         this.zip = zip;
     }
 
+    /**
+     * Retrieve the phone number of the responder
+     * @return the phone number
+     */
     public String getPhoneNumber() {
         return phoneNumber;
     }
 
+    /**
+     * Retrieve the street address of the responder
+     * @return the street address
+     */
     public String getAddress() {
         return address;
     }
 
+    /**
+     * Retrieve the state where the responder is/was located
+     * @return the state
+     */
     public String getState() {
         return state;
     }
 
+    /**
+     * Retrieve the zip code where the responder is/was located
+     * @return the zip code
+     */
     public int getZip() {
         return zip;
     }
