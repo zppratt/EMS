008ec5962740789fac4c53958d8aa1e8a25bf4b2
SunnyDjinn <guiol.matt@gmail.com>
2015-11-21 14:17:33 -0500

    Added emergencyReport class and folder

---
+++ src/com/baconfiesta/ems/models/EmergencyReport/EMSReport.java
@@ -0,0 +1,21 @@
+package com.baconfiesta.ems.models.EmergencyReport;
+
+import ...
+
+/**
+ * A report generator class in the system to create different kind of reports
+ * */
+public class Report {
+    private final String recordTemplateFilename = "Template_Record.xls";
+    private final String globalTemplateFilename = "Template_Stats.xls";
+
+    public File generateRecordReport(EmergencyRecord emergencyRecord) {
+
+
+    }
+
+    public File generateGlobalReport(EmergencyRecord[] emergencyRecords) {
+
+    }
+
+}
